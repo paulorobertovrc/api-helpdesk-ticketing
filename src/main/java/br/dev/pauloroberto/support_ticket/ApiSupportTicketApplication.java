6edf209841e14d8666131407eb5f3ad1ecdea31f
Paulo Roberto Vieira Ribeiro Cavalcanti <paulor.vrcavalcanti@gmail.com>
2023-01-13 08:21:07 -0400

    First commit

---
+++ src/main/java/br/dev/pauloroberto/support_ticket/ApiSupportTicketApplication.java
@@ -0,0 +1,13 @@
+package br.dev.pauloroberto.support_ticket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSupportTicketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiSupportTicketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/dev/pauloroberto/support_ticket/ApiSupportTicketApplicationTests.java
@@ -0,0 +1,13 @@
+package br.dev.pauloroberto.support_ticket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSupportTicketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
