8bfa9a45e15978599edc762c9617071c02a3969f
Paulo Roberto Vieira Ribeiro Cavalcanti <paulor.vrcavalcanti@gmail.com>
2023-01-14 16:40:47 -0400

    TicketRepository

--- src/main/java/br/dev/pauloroberto/support_ticket/repository/TicketRepository.java
+++ src/main/java/br/dev/pauloroberto/support_ticket/repository/TicketRepository.java
@@ -1,9 +1,15 @@
 package br.dev.pauloroberto.support_ticket.repository;
 
 import br.dev.pauloroberto.support_ticket.model.ticket.Ticket;
+import br.dev.pauloroberto.support_ticket.model.ticket.TicketStatus;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TicketRepository extends CrudRepository<Ticket, Long> {
+    List<Ticket> findByUserId(Long userId);
+    List<Ticket> findByUserName(String userName);
+    List<Ticket> findByStatus(TicketStatus status);
 }
