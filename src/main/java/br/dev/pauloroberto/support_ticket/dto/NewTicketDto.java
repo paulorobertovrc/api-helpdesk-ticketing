a9db7669366fcb90c4173d429977e9895bf3634e
Paulo Roberto Vieira Ribeiro Cavalcanti <paulor.vrcavalcanti@gmail.com>
2023-01-15 10:03:44 -0400

    Added annotations and comments

--- src/main/java/br/dev/pauloroberto/support_ticket/dto/NewTicketDto.java
+++ src/main/java/br/dev/pauloroberto/support_ticket/dto/NewTicketDto.java
@@ -1,6 +1,9 @@
 package br.dev.pauloroberto.support_ticket.dto;
 
 import br.dev.pauloroberto.support_ticket.model.ticket.Ticket;
+import br.dev.pauloroberto.support_ticket.model.ticket.TicketCategory;
+import br.dev.pauloroberto.support_ticket.utils.ValueOfEnum;
+import jakarta.validation.constraints.NotBlank;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
@@ -8,20 +11,28 @@ import java.util.stream.Collectors;
 
 public record NewTicketDto(
     Long id,
+    @NotBlank
     String title,
+    @NotBlank
     String description,
+    @NotBlank @ValueOfEnum(enumClass = TicketCategory.class)
     String ticketCategory
 ) {
-    public NewTicketDto(@NotNull String title, @NotNull String description, @NotNull String ticketCategory) {
+    public NewTicketDto(String title, String description, String ticketCategory) {
         this(null, title, description, ticketCategory);
+
+        // the id is null because it is a new ticket
+        // this same dto will be used to return the ticket, but with the id from the database
+        // the id
     }
 
-    public NewTicketDto(Ticket ticket) {
+    public NewTicketDto(@NotNull Ticket ticket) {
+        // This constructor is used by the static method List<TicketDto> from(List<Ticket> tickets)
         this(
                 ticket.getId(),
                 ticket.getTitle(),
                 ticket.getDescription(),
-                ticket.getTicketCategory().name()
+                ticket.getTicketCategory().toString()
         );
     }
 
