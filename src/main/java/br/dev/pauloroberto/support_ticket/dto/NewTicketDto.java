d7cf39ba4f7b8383073a32c0c2a331d013909c55
Paulo Roberto Vieira Ribeiro Cavalcanti <paulor.vrcavalcanti@gmail.com>
2023-01-17 12:55:09 -0400

    Removed constructor

--- src/main/java/br/dev/pauloroberto/support_ticket/dto/NewTicketDto.java
+++ src/main/java/br/dev/pauloroberto/support_ticket/dto/NewTicketDto.java
@@ -5,7 +5,6 @@ import br.dev.pauloroberto.support_ticket.utils.ValueOfEnum;
 import jakarta.validation.constraints.NotBlank;
 
 public record NewTicketDto(
-    Long id,
     @NotBlank
     String title,
     @NotBlank
@@ -13,10 +12,4 @@ public record NewTicketDto(
     @NotBlank @ValueOfEnum(enumClass = TicketCategory.class)
     String category
 ) {
-    public NewTicketDto(String title, String description, String category) {
-        this(null, title, description, category);
-        // the id is null because it is a new ticket
-        // this same dto will be used to return the ticket, but with the id from the database
-        // the id
-    }
 }
