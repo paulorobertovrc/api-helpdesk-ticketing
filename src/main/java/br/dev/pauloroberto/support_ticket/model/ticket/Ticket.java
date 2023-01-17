0c3b0f9d55bffebd65400e109837b32ce6a92daf
Paulo Roberto Vieira Ribeiro Cavalcanti <paulor.vrcavalcanti@gmail.com>
2023-01-17 12:57:29 -0400

    Added new field and methods

--- src/main/java/br/dev/pauloroberto/support_ticket/model/ticket/Ticket.java
+++ src/main/java/br/dev/pauloroberto/support_ticket/model/ticket/Ticket.java
@@ -4,6 +4,7 @@ import br.dev.pauloroberto.support_ticket.model.Answer;
 import br.dev.pauloroberto.support_ticket.model.user.User;
 import jakarta.persistence.*;
 import lombok.Data;
+import org.jetbrains.annotations.NotNull;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -19,6 +20,12 @@ public class Ticket {
     private String title;
     private String description;
     private LocalDateTime createdAt = LocalDateTime.now();
+    private LocalDateTime updatedAt;
+    private LocalDateTime closedAt;
+    private LocalDateTime deletedAt;
+    private Long closedBy;
+    private Long deletedBy;
+    private boolean deleted = false;
     @Enumerated(EnumType.STRING)
     private TicketCategory category;
     @Enumerated(EnumType.STRING)
@@ -29,4 +36,33 @@ public class Ticket {
     private User user;
     @OneToMany(mappedBy = "ticket")
     private List<Answer> answers = new ArrayList<>();
+
+    public void addAnswer(Answer answer) {
+        answers.add(answer);
+    }
+
+    public void removeAnswer(Answer answer) {
+        answers.remove(answer);
+    }
+
+    public void closeTicket(Long closedBy) {
+        this.status = TicketStatus.CLOSED;
+        this.closedAt = LocalDateTime.now();
+        this.closedBy = closedBy;
+    }
+
+    public void deleteTicket(Long deletedBy) {
+        this.status = TicketStatus.DELETED;
+        this.deletedAt = LocalDateTime.now();
+        this.deletedBy = deletedBy;
+        this.deleted = true;
+    }
+
+    public void updateTicket(@NotNull Ticket ticket) {
+        this.title = ticket.getTitle();
+        this.description = ticket.getDescription();
+        this.category = ticket.getCategory();
+        this.priority = ticket.getPriority();
+        this.updatedAt = LocalDateTime.now();
+    }
 }
