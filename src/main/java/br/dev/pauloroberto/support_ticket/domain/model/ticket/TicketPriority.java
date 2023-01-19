38830b9472932cd4593ad8a06ba7433bb09fe7a5
Paulo Roberto Vieira Ribeiro Cavalcanti <paulor.vrcavalcanti@gmail.com>
2023-01-19 00:28:36 -0400

    Added TicketPriority

---
+++ src/main/java/br/dev/pauloroberto/support_ticket/domain/model/ticket/TicketPriority.java
@@ -0,0 +1,7 @@
+package br.dev.pauloroberto.support_ticket.domain.model.ticket;
+
+public enum TicketPriority {
+    LOW,
+    NORMAL,
+    HIGH
+}
--- src/main/java/br/dev/pauloroberto/support_ticket/model/ticket/TicketPriority.java
+++
@@ -1,7 +0,0 @@
-package br.dev.pauloroberto.support_ticket.model.ticket;
-
-public enum TicketPriority {
-    LOW,
-    NORMAL,
-    HIGH
-}
