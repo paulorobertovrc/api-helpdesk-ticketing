be48b097174d5ddf4786710e60b81d75b6fd69ba
Paulo Roberto Vieira Ribeiro Cavalcanti <paulor.vrcavalcanti@gmail.com>
2023-01-19 00:21:02 -0400

    Reorganized classes

--- src/main/resources/db/migration/V1__create-table-tickets-users-answers.sql
+++ src/main/resources/db/migration/V1__create-table-tickets-users-answers.sql
@@ -2,9 +2,16 @@ CREATE TABLE tickets (
     id BIGINT NOT NULL AUTO_INCREMENT,
     title VARCHAR(255) NOT NULL,
     description TEXT NOT NULL,
-    ticketCategory ENUM('bug', 'feature', 'support', 'other') NOT NULL,
-    createdAt DATETIME NOT NULL,
-    status ENUM('open', 'in_progress', 'solved' , 'closed') NOT NULL DEFAULT 'open',
+    created_at DATETIME NOT NULL,
+    updated_at DATETIME DEFAULT NULL,
+    closed_at DATETIME DEFAULT NULL,
+    deleted_at DATETIME DEFAULT NULL,
+    cloded_by BIGINT DEFAULT NULL,
+    deleted_by BIGINT DEFAULT NULL,
+    deleted boolean DEFAULT false,
+    ticketCategory ENUM('BUG', 'FEATURE', 'SUPPORT', 'OTHER') NOT NULL,
+    status ENUM('OPEN', 'IN_PROGRESS', 'SOLVED' , 'CLOSED') NOT NULL DEFAULT 'OPEN',
+    priority ENUM('LOW', 'NORMAL', 'HIGH') NOT NULL DEFAULT 'NORMAL',
     user BIGINT NOT NULL,
     answers BIGINT,
 
@@ -14,8 +21,12 @@ CREATE TABLE tickets (
 CREATE TABLE answers (
     id BIGINT NOT NULL AUTO_INCREMENT,
     description TEXT NOT NULL,
-    createdAt DATETIME NOT NULL,
     ticket BIGINT NOT NULL,
+    createdAt DATETIME NOT NULL,
+    updatedAt DATETIME DEFAULT NULL,
+    deletedAt DATETIME DEFAULT NULL,
+    deletedBy BIGINT DEFAULT NULL,
+    deleted boolean DEFAULT false,
     user BIGINT NOT NULL,
     solution boolean NOT NULL DEFAULT false,
 
@@ -25,11 +36,18 @@ CREATE TABLE answers (
 CREATE TABLE users (
     id BIGINT NOT NULL AUTO_INCREMENT,
     name VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
     tickets BIGINT,
     answers BIGINT,
-    role ENUM('admin', 'user', 'moderator') NOT NULL DEFAULT 'user',
+    roles BIGINT,
+
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE roles (
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    user_roles ENUM('ROLE_ADMIN', 'ROLE_USER', 'ROLE_MODERADOR') NOT NULL DEFAULT 'ROLE_USER',
 
     PRIMARY KEY (id)
 );
--- src/main/resources/db/migration/V3__alter-table-tickets-drop-repeated-columns.sql
+++
@@ -1,7 +0,0 @@
-ALTER TABLE helpdesk.tickets
-    DROP COLUMN closedAt,
-    DROP COLUMN closedBy,
-    DROP COLUMN createdAt,
-    DROP COLUMN deletedAt,
-    DROP COLUMN deletedBy,
-    DROP COLUMN updatedAt;
